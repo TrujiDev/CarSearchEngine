62a6d45922d3c2feea829d990dc2c4b2d2e28137
Eduar Trujillo <eetgrisales@gmail.com>
2023-11-09 09:54:03 -0500

    Added comments for better understanding of the code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Selecting HTML elements
 const year = document.querySelector('#year');
 const brand = document.querySelector('#brand');
 const minimumPrice = document.querySelector('#minimum');
@@ -7,9 +8,11 @@ const transmission = document.querySelector('#transmission');
 const color = document.querySelector('#color');
 const result = document.querySelector('#result');
 
+// Setting date range for the year select
 const maxDate = new Date().getFullYear();
 const minDate = maxDate - 10;
 
+// Object to store search criteria
 const dataSearch = {
 	brand: '',
 	year: '',
@@ -20,54 +23,50 @@ const dataSearch = {
 	color: '',
 };
 
+// Event listener for DOMContentLoaded to initialize the page
 document.addEventListener('DOMContentLoaded', () => {
 	showCars(cars);
 	fillSelect();
 });
 
+// Event listeners for each filter option
 brand.addEventListener('change', event => {
 	dataSearch.brand = event.target.value;
-
 	filterCar();
 });
 
 year.addEventListener('change', event => {
 	dataSearch.year = parseInt(event.target.value);
-
 	filterCar();
 });
 
 minimumPrice.addEventListener('change', event => {
 	dataSearch.minimumPrice = event.target.value;
-
 	filterCar();
 });
 
 maximumPrice.addEventListener('change', event => {
 	dataSearch.maximumPrice = event.target.value;
-
 	filterCar();
 });
 
 doors.addEventListener('change', event => {
 	dataSearch.doors = parseInt(event.target.value);
-
 	filterCar();
 });
 
 transmission.addEventListener('change', event => {
 	dataSearch.transmission = event.target.value;
-
 	filterCar();
 });
 
 color.addEventListener('change', event => {
-    dataSearch.color = event.target.value;
-    console.log(dataSearch);
-
+	dataSearch.color = event.target.value;
+	console.log(dataSearch);
 	filterCar();
 });
 
+// Function to display cars in the result section
 function showCars(cars) {
 	cleanHTML();
 
@@ -83,12 +82,14 @@ function showCars(cars) {
 	});
 }
 
+// Function to remove all child elements from the result section
 function cleanHTML() {
 	while (result.firstChild) {
 		result.removeChild(result.firstChild);
 	}
 }
 
+// Function to fill the year select with options
 function fillSelect() {
 	for (let i = maxDate; i >= minDate; i--) {
 		const option = document.createElement('option');
@@ -98,91 +99,111 @@ function fillSelect() {
 	}
 }
 
+// Function to apply filters and display results
 function filterCar() {
-	const result = cars
-		.filter(filterBrand)
-		.filter(filterYear)
-		.filter(filterMinimumPrice)
-		.filter(filterMaximumPrice)
-		.filter(filterDoors)
-		.filter(filterTransmission)
-		.filter(filterColor);
+    // Apply successive filters to the list of cars based on search criteria
 
+    const result = cars
+        .filter(filterBrand)           // Filter by brand
+        .filter(filterYear)            // Filter by year
+        .filter(filterMinimumPrice)    // Filter by minimum price
+        .filter(filterMaximumPrice)    // Filter by maximum price
+        .filter(filterDoors)           // Filter by number of doors
+        .filter(filterTransmission)    // Filter by transmission type
+        .filter(filterColor);          // Filter by color
+
+    // If there are filtered results, display them; otherwise, show a no-result message
     if (result.length) {
-        showCars(result);
+        showCars(result);  // Display filtered cars
     } else {
-        noResult();
+        noResult();        // Display a message indicating no results
     }
 }
 
+
+// Function to display a message when there are no results
 function noResult() {
-    cleanHTML();
+	cleanHTML();
 
-    const noResult = document.createElement('DIV');
-    noResult.classList.add('alert', 'error');
-    noResult.textContent = 'No results, try with other search terms';
-    result.appendChild(noResult);
+	const noResult = document.createElement('DIV');
+	noResult.classList.add('alert', 'error');
+	noResult.textContent = 'No results, try with other search terms';
+	result.appendChild(noResult);
 }
 
+// Functions to filter cars based on search criteria
+
+// Function to filter by brand
 function filterBrand(car) {
-	const { brand } = dataSearch;
+    const { brand } = dataSearch;
 
-	if (brand) {
-		return car.brand === brand;
-	}
-	return car;
+    if (brand) {
+        return car.brand === brand;
+    }
+    return car;
 }
 
+// Function to filter by year
 function filterYear(car) {
-	const { year } = dataSearch;
+    const { year } = dataSearch;
 
-	if (year) {
-		return car.year === year;
-	}
-	return car;
+    if (year) {
+        return car.year === year;
+    }
+    return car;
 }
 
+// Function to filter by minimum price
 function filterMinimumPrice(car) {
-	const { minimumPrice } = dataSearch;
+    const { minimumPrice } = dataSearch;
 
-	if (minimumPrice) {
-		return car.price >= minimumPrice;
-	}
-	return car;
+    // If a minimum price is defined,
+    // compare it with the price of the car
+    if (minimumPrice) {
+        return car.price >= minimumPrice;
+    }
+    return car;
 }
 
+// Function to filter by maximum price
 function filterMaximumPrice(car) {
-	const { maximumPrice } = dataSearch;
+    const { maximumPrice } = dataSearch;
 
-	if (maximumPrice) {
-		return car.price <= maximumPrice;
-	}
-	return car;
+    // If a maximum price is defined,
+    // compare it with the price of the car
+    if (maximumPrice) {
+        return car.price <= maximumPrice;
+    }
+    return car;
 }
 
+// Function to filter by number of doors
 function filterDoors(car) {
-	const { doors } = dataSearch;
+    const { doors } = dataSearch;
 
-	if (doors) {
-		return car.doors === doors;
-	}
-	return car;
+    if (doors) {
+        return car.doors === doors;
+    }
+    return car;
 }
 
+// Function to filter by transmission type
 function filterTransmission(car) {
-	const { transmission } = dataSearch;
+    const { transmission } = dataSearch;
 
-	if (transmission) {
-		return car.transmission === transmission;
-	}
-	return car;
+    if (transmission) {
+        return car.transmission === transmission;
+    }
+    return car;
 }
 
+// Function to filter by color
 function filterColor(car) {
-	const { color } = dataSearch;
+    const { color } = dataSearch;
 
-	if (color) {
-		return car.color === color;
-	}
-	return car;
+    if (color) {
+        return car.color === color;
+    }
+    return car;
 }
+
